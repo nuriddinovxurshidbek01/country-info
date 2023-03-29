1fb76b5c5fcbb56ed9a01bf65dc887c1fefc8066
nuriddinovxurshidbek04 <nuriddinovxurshidbek04@gmail.com>
2023-03-29 16:56:07 +0500

    start

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 "use strict";
 
 let wrapper = $(".wrapper");
+let elSearch = $(".searchMain")
 
-const dataInfo = fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+const dataInfo = fetch("https://restcountries.com/v3.1/all")
     .then(res => res.json())
     .then((data) => {
 
         renderUi(data);
         console.log(data);
+        // search(data)
     })
 
 
 
 function renderUi(array = data) {
-    array.forEach(evt => {
+    array.forEach(item => {
         let card = document.createElement("div");
         card.setAttribute("class", "card");
         card.innerHTML = `
-                          <img src="${evt.flags.png}" class="card-img-top mainImg" alt="flag">
+                          <img src="${item.flags.png}" class="card-img-top mainImg" alt="flag">
                             <div class="card-body p-2">
-                                <h class="card-title  fs-2">${evt.name.common}</h>
-                                <p class="card-text">${evt.name.official}</p>
-                                <buttom class="btn btn-primary">Go somewhere</buttom>
+                                <h class="card-title  fs-2">${item.name.common}</h>
+                                <p class="card-text"> <strong>Population: </strong> ${item.population} </p>
+                                <p class="card-text"> <strong>Region: </strong> ${item.region} </p>
+                                <p class="card-text"> <strong>Capital: </strong> ${item.capital} </p>
+                                <buttom class="btn btn-primary">More Info</buttom>
                             </div>
         `
             // <p class="card-text">${evt.flags.alt? evt.flags.alt :"The flag of North Macedonia has a red field, at the center of which is a golden-yellow sun with eight broadening rays that extend to the edges of the field."} </p>
@@ -29,3 +33,17 @@ function renderUi(array = data) {
 
     });
 }
+
+// function search(array) {
+//     elSearch.addEventListener("keyup", (evt) => {
+//         evt.preventDefault();
+
+//         let value = evt.target.value;
+//         wrapper.innerHTML = "";
+//     })
+
+//     let newItem = array.filter(item => {
+//         return item.title.includes(value);
+//     })
+//     renderUi(newItem);
+// }
